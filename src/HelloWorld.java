fe2da9dd5b84d3406895eaffaecf51d245c328b6
TEKHD0 <tekhd0@gmail.com>
2020-09-23 12:37:54 -0400

    k

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main (String[] args){
+
+
+        System.out.println("Hello World!");
+        System.out.println("We are Trevor and Vincent, writing our first Java code\n" +
+                "in Grayson Howard’s CS1121 Lab at Michigan Tech!");
+
+    }
+}
